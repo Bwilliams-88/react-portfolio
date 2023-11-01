7bb92edc71948ce0ff2329b7c178d7884aa8e7de
Brandon Williams <brandonwilliams.88@yahoo.com>
2023-11-01 18:49:12 -0500

    added 'boiler plate codes and code to NavTabs.jsx'

---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import ReactDOM from 'react-dom/client'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+// import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Portfolio from './pages/Portfolio';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/resume',
+        element: <Resume />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
