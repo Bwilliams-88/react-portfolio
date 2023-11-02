47cc3be81ae8b84a687f78a56c296adb6c218ff5
Brandon Williams <brandonwilliams.88@yahoo.com>
2023-11-02 15:29:14 -0500

    all files should be complete, working on getting it to load correctly

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App';
 // import Home from './pages/Home';
+import Error from './pages/Error'
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Resume from './pages/Resume';
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,10 @@
+export default function Portfolio() {
+    return (
+      <div>
+        <h1>Portfolio</h1>
+        <p>
+          {/* Insert portfolio */}
+        </p>
+      </div>
+    );
+  }
